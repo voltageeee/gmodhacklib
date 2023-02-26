e0783843dd60f8291f39a66ed672939b06ad0970
Ilya <60658899+SpawnCodegmd@users.noreply.github.com>
2023-02-26 17:33:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <title>3glib, библиотека GMOD-читов</title>
+
+    
+
+    
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+  </head>
+  <body>
+    
+<header>
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">О сайте</h4>
+          <p class="text-white">Сайт сделал spawncode. При поддержке: <a href="https://discord.gg/GAsQQ4tSqs" class="text-white"> 3gshop</p>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand d-flex align-items-center">
+        <strong>3glibrary</strong>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Библиотека бесплатных gmod-читов</h1>
+        <p class="lead text-muted">Тут вы можете найти бесплатные читы для гмода. Библиотека будет регулярно пополняться. Пока что она содержит только читы, которые использовал Urbanichka.
+        Домен и хостинг оплачивается из кармана создателя сайта. Если хотите - можете поддержать проект по кнопке ниже.</p>
+        <p>
+          <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Source-код сайта</a>
+          <a href="https://qiwi.com/payment/form/99999?extra[%27accountType%27]=nickname&extra[%27account%27]=SPCODE99" class="btn btn-primary my-2">Поддержать сайт</a>
+          <a href="https://brokencore.club" class="btn btn-primary my-2">Источник всех читов</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <iframe width="420" height="225" src="https://www.youtube.com/embed/NZWg-0ZUviA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">CRANIUM: Самый популярный чит на данный момент. Лучший чит для игры.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Скачать</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">Virustotal</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">CFG</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <iframe width="420" height="225" src="https://www.youtube.com/embed/aF-JjlFC2RM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">ONETAP: Один из лучших бесплатных читов. Ренеймнулся на CRANIUM.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Скачать</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">Virustotal</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">CFG</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <iframe width="420" height="225" src="https://www.youtube.com/embed/AEAUjhNQS9g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">MEMORIAM: Бесплатный чит с красивым меню. Статус: Undetected</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Скачать</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">Virustotal</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">CFG</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <iframe width="420" height="225" src="https://www.youtube.com/embed/CXsscJhxLfQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">CFF: Бесплатный, проверенный временим чит. Статус: Undetected.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Скачать</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">Virustotal</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">CFG</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <iframe width="420" height="225" src="https://www.youtube.com/embed/-1qU2UAcZzU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">NEVERPIVO. Бесплатный чит на гмод с менюшкой неверлуза. Статус: Undetected.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="https://github.com/SpawnCodegmd" class="btn btn-primary my-2">Скачать</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">Virustotal</a>
+                  <a href="https://brokencore.club" class="btn btn-secondary my-2">CFG</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+
+</main>
+
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
